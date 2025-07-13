d147ed168c9ad4a4bb9aa57176110fd5bc622efc
Janderson <jandersonjunio@hotmail.com>
2025-07-13 12:55:22 -0300

    usuario repositorio atualizado

--- backend/src/main/java/com/gravacao/repository/UsuarioRepository.java
+++ backend/src/main/java/com/gravacao/repository/UsuarioRepository.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+    @Query("SELECT u FROM Usuario u LEFT JOIN FETCH u.roles WHERE u.id = :id")
     boolean existsByEmail(String email);
 
     boolean existsByCpf(String cpf);
